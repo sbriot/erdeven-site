ffdea52f95f8f43c3c02f3f75e53a9177289c798
Sébastien <sebastien.briot@etu.utc.fr>
2022-06-28 07:17:07 +0200

    update address

--- config.js
+++ config.js
@@ -36,6 +36,6 @@ module.exports = {
   phone_name : 'Jean-François',
   phone: '06.62.83.13.29',
   phonelink: '0033662831329',
-  address: 'Boulevard de l\' Atlantique au Spot, lieu d\'activité du bien être, 56410 Erdeven',
+  address: '4 Boulevard de l\' Atlantique, lieu d\'activité du bien être, 56410 Erdeven',
 
 };
