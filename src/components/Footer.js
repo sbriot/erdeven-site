f9d3e4acabe82c45366ef43d4b6a291411e199a5
sebastien <sbbriot@gmail.com>
2023-02-06 18:54:05 +0100

    change phone number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,13 +16,13 @@ export default function Footer() {
           <div className="col-6 col-12-medium">
             <ul className="contact">
               <li className="fa-home">{config.address}</li>
-              <li className="fa-phone">{config.phone_name} : <a href="tel:033662831329">{config.phone}</a></li>
+              <li className="fa-phone">{config.phone_name} : <a href="tel:0033661043367">{config.phone}</a></li>
 
             </ul>
           </div>
         </div>
         <ul className="copyright">
-          <li>&copy; Ecole de Natation Erdeven - 2022 </li>
+          <li>&copy; Ecole de Natation Erdeven - 2023 </li>
           <li>
             Design: <a href="http://html5up.net">HTML5 UP</a>
           </li>
